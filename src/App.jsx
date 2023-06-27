aae375b485ebe6c7572b96866de935c987c256dd
jimdanielw <135315915+jimdanielw@users.noreply.github.com>
2023-06-27 10:27:32 +0800

    patch-6 Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      <h2>Test heading 2</h2>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
